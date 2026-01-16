331c0ee01d09dc0fdae301b6974a34d0284584eb
myosee <okunevmx@mail.ru>
2026-01-16 13:53:37 +0300

    jquerry?

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ var modal = document.querySelector('.modal-container');
 var closeButton = document.querySelector('.close');
 var modalTriggers = document.querySelectorAll('.open');
 
-var openModal = function() {
-    modal.classList.add('is-open')
-   }
-   var closeModal = function() {
-    modal.classList.remove('is-open')
-   }
+var openModal = function() { modal.classList.add('is-open') }
+var closeModal = function() { modal.classList.remove('is-open') }
    modalTriggers.forEach(function(item) { 
     item.addEventListener('click', openModal);
    })
-   closeButton.addEventListener('click', closeModal)
+   closeButton.addEventListener('click', closeModal);
 
-
-$(() => {
+$(function(){
     $(window).scroll(function () {
         if ($(this).scrollTop() != 0) {
             $('#toTop').fadeIn();
